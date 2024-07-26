09990d38647712c3c6c148c2320b4dad3d01a90c
Степан Рудаков <19stepan94@mail.ru>
2024-07-26 18:04:39 +0500

    начал писать функционал итоговой

--- game.js
+++ game.js
@@ -1,1 +1,51 @@
 'use strict';
+
+(() => {
+
+  let userMarbles = 5;
+  let botMarbles = 5;
+
+  const evenOrOdd = (number) => {
+    if (number % 2 === 0) {
+      return 'четное';
+  } else {
+      return 'нечетное';
+  }
+};
+
+const playRound = () => {
+  alert('у вас ${userMarbles} шариков. У бота ${botMarbles} шариков');
+
+  if(userMarbles <= 0) {
+    alert("У вас закончились шарики. Бот выиграл");
+    return;
+  } else if(botMarbles <= 0){
+    alert("У бота закончились шарики. Вы победили");
+    return;
+  }
+}
+
+while(true) {
+  guessNumber = prompt('Введите количество шариков, которое вы хотите загадать (1-${userMarbles}):');
+  if(guessNumber >= 1 && guessNumber <= userMarbles) {
+    break;
+  } else {
+    alert('Введите число от 1 до ${playerMarbles}.');
+  }
+}
+
+const botGuess = 
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
