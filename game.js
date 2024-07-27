6f298d5bf6eb3480a9a8b77bf1d9e579931ec31e
Степан <19stepan94@mail.ru>
2024-07-27 15:04:53 +0500

    пишу первые функции

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 'use strict';
 
-(() => {
+const createMarbleGame = () => {
 
   let userMarbles = 5;
   let botMarbles = 5;
@@ -14,7 +14,7 @@
 };
 
 const playRound = () => {
-  alert('у вас ${userMarbles} шариков. У бота ${botMarbles} шариков');
+  alert(`у вас ${userMarbles} шариков. У бота ${botMarbles} шариков`);
 
   if(userMarbles <= 0) {
     alert("У вас закончились шарики. Бот выиграл");
@@ -25,27 +25,61 @@ const playRound = () => {
   }
 }
 
-while(true) {
-  guessNumber = prompt('Введите количество шариков, которое вы хотите загадать (1-${userMarbles}):');
-  if(guessNumber >= 1 && guessNumber <= userMarbles) {
-    break;
-  } else {
-    alert('Введите число от 1 до ${playerMarbles}.');
+const guessNumber = (guessNumberrr) => {
+  while(true) {
+    guessNumberrr = prompt(`Введите количество шариков, которое вы хотите загадать от 1 до ${userMarbles}:`);
+    if(guessNumber >= 1 && guessNumber <= this.userMarbles) {
+      return;
+    } else {
+      alert(`Введите число от 1 до ${userMarbles}`);
+    }
   }
 }
 
-const botGuess = 
+// guessNumber();
+let botGuess;
+if (Math.random() < 0.5) {
+  botGuess = 'четное';
+  alert('Бот думает, что ваше число четное.');
+  return playRound();
+} else {
+  botGuess = 'нечетное';
+  alert('Бот думает, что ваше число нечетное.');
+  return playRound();
+};
+
+
+const actualParity = evenOrOdd(guessNumber);
+        if (botGuess === actualParity) {
+            console.log(`Бот угадал! Он забирает ${guessNumber} шариков.`);
+            userMarbles -= guessNumber;
+            botMarbles += guessNumber;
+        } else {
+            console.log(`Бот не угадал! Вы забираете ${guessNumber} шариков.`);
+            userMarbles += guessNumber;
+            botMarbles -= guessNumber;
+        }
+    
+
+  
+    return {
+        playRound: playRound(),
+        getUserMarbles: () => userMarbles,
+        getBotMarbles: () => botMarbles
+    };
+
+
+const marbleGame = createMarbleGame();
+
+while (marbleGame.getUserMarbles() > 0 && marbleGame.getBotMarbles() > 0) {
+    marbleGame.playRound();
+};
 
 
 
 
 
 
+};
 
-
-
-
-
-
-
-});
+createMarbleGame();
