646eca8de846adc9e56f999daa63e09949431d1b
Степан <19stepan94@mail.ru>
2024-07-28 14:19:53 +0500

    сделал 5 пунктов задания

--- game.js
+++ game.js
@@ -4,21 +4,22 @@ const createMarbleGame = () => {
 
   let userMarbles = 5;
   let botMarbles = 5;
-  let userGuessNumber;
   const evenNumber = 'четное';
   const oddNumber = 'нечетное';
+  
 
-  const evenOrOdd = (userGuessNumber) => {
-    if (userGuessNumber % 2 === 0) {
+  const evenOrOdd = (number) => {
+    if (number % 2 === 0) {
       return evenNumber;
   } else {
       return oddNumber;
   }
 };
 
+
+
 const playRound = () => {
   alert(`у вас ${userMarbles} шариков. У бота ${botMarbles} шариков`);
-  
   if(userMarbles <= 0) {
     alert("У вас закончились шарики. Бот выиграл");
     return;
@@ -26,61 +27,56 @@ const playRound = () => {
     alert("У бота закончились шарики. Вы победили");
     return;
   }
-  return guessNumber();
+
+
+let userGuessNumber;
+while(true) {
+  userGuessNumber = parseInt(prompt(`Введите количество шариков, которое вы хотите загадать от 1 до ${userMarbles}:`));
+  if (userGuessNumber >= 1 && userGuessNumber <= userMarbles) {
+    break;
+} else {
+    alert(`Введите число от 1 до ${userMarbles}.`);
+}
 }
 
 
-const guessNumber = () => {
-  while(true) {
-    userGuessNumber = parseInt(prompt(`Введите количество шариков, которое вы хотите загадать от 1 до ${userMarbles}:`));
-    if(userGuessNumber >= 1 && userGuessNumber <= userMarbles) {
-      return;
-    } else {
-      alert(`Введите число от 1 до ${userMarbles}`);
-    }
-  }
-} 
-
-guessNumber();
-let botGuess;
-if (Math.random() < 0.5) {
-  botGuess = evenNumber;
-  alert(`Бот думает, что ваше число ${evenNumber}.`);
-  
-} else {
-  botGuess = oddNumber;
-  alert(`Бот думает, что ваше число ${oddNumber}.`);
-  
-};
-
-
 const actualParity = evenOrOdd(userGuessNumber);
+
+const botGuess = Math.random() < 0.5 ? evenNumber : oddNumber;
+        alert(`Бот думает, что ваше число ${botGuess}.`);
+ 
   if (botGuess === actualParity) {
     alert(`Бот угадал! Он забирает ${userGuessNumber} шариков.`);
-    userMarbles = userMarbles - userGuessNumber;
+    userMarbles -= userGuessNumber;
     botMarbles += userGuessNumber;
 } else {
     alert(`Бот не угадал! Вы забираете ${userGuessNumber} шариков.`);
     userMarbles += userGuessNumber;
-    botMarbles = botMarbles - userGuessNumber;
+    botMarbles -= userGuessNumber;
 }
- return playRound();
-        
-    
+
+return playRound();
+
+};
+
+while (userMarbles > 0 && botMarbles > 0) {
+    playRound();
+};
+
+};
 
 
 
 const marbleGame = createMarbleGame();
 
-while (marbleGame.getUserMarbles() > 0 && marbleGame.getBotMarbles() > 0) {
-    marbleGame.playRound();
-};
 
 
 
 
 
 
-};
 
-createMarbleGame();
+
+
+
+
